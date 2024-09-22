3f47986b4d3f0c9c52af6f2bd079e6ebc70f2f17
Eliesangdev <eliesanginga@gmail.com>
2024-09-22 13:46:09 +0300

    Add files via upload

---
+++ fivebi.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string name = "ELISHA SANGINGA";//declare name as string  
+    cout << "My name is:" << name << endl;//cout my name
+    return 0;
+}
+
